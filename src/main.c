118fc5c0bcd63b755d87466f7e9325698e06fc24
Hektor <hektorzaimi.dev@gmail.com>
2026-01-15 16:47:17 +0100

    add shell tokenization of command and arguments

--- src/main.c
+++ src/main.c
@@ -2,27 +2,58 @@
 #include <string.h>
 #include "vfs.h"
 
-int main() {
+int main()
+{
     vfs_init();
     char input[100];
 
     printf("Welcome to WasiOS-2! Type 'help' for commands.\n");
 
-    while (1) {
+    while (1)
+    {
         printf("wasm_os:%s$ ", current_dir->name);
-        if (!fgets(input, 100, stdin)) break;
-        input[strcspn(input, "\n")] = 0;
-
-        if (strncmp(input, "mkdir",5) == 0) {
-            vfs_mkdir(input + 5);
-        } else if (strcmp(input, "ls") == 0) {
-            vfs_ls();
-        } else if (strcmp(input, "exit") == 0) {
+        if (!fgets(input, 100, stdin))
             break;
-        } else {
-            printf("Command not found. \n");
+        input[strcspn(input, "\r\n")] = 0;
+
+        // -== Split into Command and Argument ==-
+
+        char *cmd = strtok(input, " \r\n");
+        char *arg = strtok(NULL, " \r\n");
+
+        if (!cmd)
+            continue; // ENTER is pressed
+
+        // -== Handle commands ==-
+
+        if (strcmp(cmd, "exit") == 0)
+        {
+            break;
+        }
+        else if (strcmp(cmd, "ls") == 0)
+        {
+            vfs_ls();
+        }
+        else if (strcmp(cmd, "mkdir") == 0)
+        {
+            if (arg)
+            {
+                vfs_mkdir(arg);
+            }
+            else
+            {
+                printf("Usage: mkdir <name>\n");
+            }
+        }
+        else if (strcmp(cmd, "help") == 0)
+        {
+            printf("Commands: ls, mkdir, exit\n");
+        }
+        else
+        {
+            printf("Unknown command: %s\n", cmd);
         }
     }
-    
+
     return 0;
 }
